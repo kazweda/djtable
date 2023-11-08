241386fe9299f1b26f4e765508725d7a78dac429
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-11-08 17:40:07 +0900

    #6 ひとまずの投稿リストのカスタマイズ

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,11 @@ from .models import Post
 class IndexView(generic.TemplateView):
     template_name = 'blog/index.html'
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        posts = Post.objects.all()
+        context['num_posts'] = len(posts)
+        return context
+    
 class PostListView(generic.ListView): # generic の ListViewクラスを継承
     model = Post # 一覧表示させたいモデルを呼び出し
