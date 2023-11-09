d0303b68db1e0ff2678027df0c894c3611f72da9
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-11-09 17:04:08 +0900

    #12

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post # 追加
 
-admin.site.register(Post) # 追加
+class BlogAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(author=request.user)
+
+admin.site.register(Post, BlogAdmin) # 追加
