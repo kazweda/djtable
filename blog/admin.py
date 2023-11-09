10cdc3e540c7bb266629d911174f66cc35540ce6
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-11-09 17:32:22 +0900

    #12 superuser以外は自分のグループの投稿のみリスト表示

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from .models import Post # 追加
-
+      
 class BlogAdmin(admin.ModelAdmin):
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(author=request.user)
-
+  def get_queryset(self, request):
+      qs = super().get_queryset(request)
+      if request.user.is_superuser:
+          return qs
+      return qs.filter(author=request.user)
+    
+    
+# admin.site.unregister(User)
 admin.site.register(Post, BlogAdmin) # 追加
