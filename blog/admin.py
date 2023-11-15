06588927302f215849a6eb3acf0f437871c7dd74
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-11-15 16:11:44 +0900

    #10 adminサイトの調整

--- blog/admin.py
+++ blog/admin.py
@@ -1,15 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-# from django.contrib.auth.models import User
 from django.conf import settings
 User = settings.AUTH_USER_MODEL
-from .models import Post # 追加
-      
-# class BlogAdmin(admin.ModelAdmin):
-#   def get_queryset(self, request):
-#       qs = super().get_queryset(request)
-#       if request.user.is_superuser:
-#           return qs
-#       return qs.filter(author=request.user)
+from .models import Post
 
 admin.site.register(Post)
