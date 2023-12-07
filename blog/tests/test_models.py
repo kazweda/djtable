fb2613ae8e23ade7e6e2146dc18e6cd3880006e4
Kazuaki Ueda <kazuaki.ueda@gmail.com>
2023-12-07 13:10:40 +0900

    #14 実装の前にテストコードを整える

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -9,4 +9,14 @@ class PostTestCase(TestCase):
   def test_blog_title(self):
     post = Post.objects.get(title="title1")
     self.assertEqual(post.title, "title1")
-    
+  
+  def test_is_count_two(self):
+    posts = Post.objects.all()
+    self.assertEqual(posts.count(), 2)
+
+  def test_is_count_three(self):
+    post = Post(title='title3', text='ブログ本文3')
+    post.save()
+    posts = Post.objects.all()
+    self.assertEqual(posts.count(), 3)
+  
